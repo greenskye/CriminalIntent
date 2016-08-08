670bdd36466b08a75aefef9222c4b3baf0f39112
Brandon Harbour <brandon.harbour@hallmark.com>
2016-08-08 10:01:50 -0500

    Chapter 9 - Displaying Lists with RecyclerView

--- app/src/main/java/com/brandon/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,13 @@ package com.brandon.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by BHARBO3 on 8/5/2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
